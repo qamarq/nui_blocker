5e7a839c3aa21308a3a65aace7949655bc1735d8
Tsai <72063585+XNORGATE@users.noreply.github.com>
2021-03-08 11:56:03 +0800

    Create permissions.lua

---
+++ permissions.lua
@@ -0,0 +1,12 @@
+checkmethod = 'steam' ---- replace with 'steam' or 'SQL'
+--if checkmethod is steam 
+--will allow players in allowlist to open nui_devtool
+
+--if checkmethod is SQL 
+--will check user.group in SQL 
+--allow admin and superadmin to open nui_devtool 
+
+allowlist = {
+'steam:110000135910727',
+'steam:11000011C0FDDBE'
+}
