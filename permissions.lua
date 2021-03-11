8c56bbd7f530d103ba55b0084050f56cdab1b40f
QamarQ <79667721+QamarQ@users.noreply.github.com>
2021-03-11 09:17:36 +0100

    Update permissions.lua

--- permissions.lua
+++ permissions.lua
@@ -1,6 +1,9 @@
 checkmethod = 'steam' ---- replace with 'steam' or 'SQL'
+extendedVersionV1Final = false
 --if checkmethod is steam 
 --will allow players in allowlist to open nui_devtool
+
+-- if use es extended in version v1 final (in db save license, not steam) and checkmetode = SQL then type true in extendedVersionV1Final
 
 --if checkmethod is SQL 
 --will check user.group in SQL 
